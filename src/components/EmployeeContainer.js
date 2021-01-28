3e9e08e974dcae9379ec49087d252d6fc6f24990
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-28 14:13:01 -0800

    adds handleSort function

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -8,32 +8,66 @@ import API from '../utils/API'
 class EmployeeContainer extends Component {
     state = {
         results: [],
-        search: ""
+        search: "",        
+        isCliked: false
     }
     componentDidMount() {
         this.searchEmployees();
-      }
+        
+    }
 
     searchEmployees = () => {
         API()        
-        .then(res => this.setState({ results: res.data.results }))
-        .catch(err => console.log(err));
+        .then(res => this.setState({ results: res.data.results }))                
+        .catch(err => console.log(err));    
     };
+    
     handleInputChange = event => {
-        const name = event.target.name;
-        const value = event.target.value;
-        
+        const {name, value } = event.target;            
         this.setState({
-          [name]: value
+          [name]: value         
         });
-      };
+    };
     
       // When the form is submitted, search the Giphy API for `this.state.search`
-      handleFormSubmit = event => {
-        event.preventDefault();
-        this.searchEmployees(this.state.search);
-      };
+    handleFormSubmit = event => {
+      event.preventDefault();
+      this.searchEmployees(this.state.search);
+    };
 
+    handleSort = () => {      
+      let arr = this.state.results
+      if(!this.state.isCliked){
+        arr.sort((a, b) => {
+          let nameA = a.name.first.toUpperCase();
+          let nameB = b.name.first.toUpperCase();
+          if(nameA > nameB) {
+            return 1;
+          } else {
+            return -1
+          }
+        })
+        this.setState({
+          results: arr,
+          isCliked: true
+        })
+      } else {
+        arr.sort((a, b) => {
+          let nameA = a.name.first.toUpperCase();
+          let nameB = b.name.first.toUpperCase();
+          if(nameA > nameB) {
+            return -1;
+          } else {
+            return 1
+          }
+        })
+        this.setState({
+          results: arr,
+          isCliked: false
+        })
+      }
+      
+    }
     render(){
         return (
             <>
@@ -43,7 +77,10 @@ class EmployeeContainer extends Component {
                     handleFormSubmit={this.handleFormSubmit}
                     handleInputChange={this.handleInputChange}
                 />
-                <Table result={this.state.results}/>
+                <Table 
+                  handleSort={this.handleSort} 
+                  result={this.state.results}
+                />
             </>
         );
     }
