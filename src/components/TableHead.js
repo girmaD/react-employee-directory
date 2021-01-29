a344a814773e9533ed3f8a0c782720a9fedf1d8e
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-29 12:42:33 -0800

    includes the up and down arrow next to name col of the table

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -6,7 +6,7 @@ function TableHead(props) {
         <thead className="bg-dark text-white">
             <tr>
                 <th scope="col">Image</th>
-                <th scope="col"><button onClick={props.handleSort} className="btn btn-dark">Name &#x0005E;</button></th>
+                <th scope="col"><button onClick={props.handleSort} className="btn btn-dark">Name &#x02195;</button></th>
                 <th scope="col">Phone</th>
                 <th scope="col">Email</th>
                 <th scope="col">DOB</th>
