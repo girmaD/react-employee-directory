ffae001e16b764c0c44bdc691b6dca9018dee6be
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-29 12:44:31 -0800

    makes the results from the API call are in later for filtering purposes by filtering by country

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASEURL = 'https://randomuser.me/api/?results=75';
+const BASEURL = 'https://randomuser.me/api/?results=75&nat=us,dk,fr,gb';
 
 const API = () => {
     return axios.get(BASEURL)
