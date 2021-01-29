7ffd9a055531bb884b66b5348448b1cb215f9420
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-29 12:41:16 -0800

    includes functions that run  onSubmit and onChange

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 function SearchFrom(props) {
-    console.log(props.search)
+    // console.log(props.filter)
     return (       
-        <div className="d-flex justify-content-center my-2 shadow p-3 mb-5 bg-white rounded">                
+        <div className="d-flex justify-content-center my-2 shadow p-3 mb-5 bg-white rounded">  
+            <form onSubmit={props.handleFormSubmit}>              
                 <input 
                     type="text" 
                     id="search" 
                     placeholder="search"
                     name="search"
-                    onChange={props.handleInputChange}
-                    onSubmit={props.handleFormSubmit}
+                    onChange={props.handleInputChange}                    
                     value={props.search}
-                />                            
+                />  
+            </form>                          
         </div>          
     );
 }
