7cc6fbec9110ff3052110f3bdb81c036d205b94a
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-29 14:13:26 -0800

    includes dayjs to format date of birth on the table

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,17 +1,17 @@
-function TableBody(props){
-    
+import dayjs from 'dayjs';
+
+function TableBody(props){    
     return (       
         <tbody>
-            {props.result.map(item=> (
+            {props.result.map(item=> (                
                 <tr  key={item.login.salt} >                  
                     <td><img alt={item.name.first} src={item.picture.thumbnail} /></td>
                     <td>{item.name.first} {item.name.last} </td>
                     <td>{item.cell}</td>
                     <td>{item.email}</td>
-                    <td>{item.dob.date}</td>
+                    <td>{dayjs(item.dob.date).format('MM/DD/YYYY')}</td>
                 </tr> 
-            ))}
-                       
+            ))}                       
         </tbody>        
     );
 }
